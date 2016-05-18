ff8ec81a412ed53c9cd1e59d67330813f29eba32
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-05-18 13:32:04 -0400

    updated app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,4 @@
 /* main JavaScript file */
-
 // IIFE - Immediately Invoked Function Expression
 (function(){
     "use strict";
@@ -8,13 +7,37 @@
     // code codes in here 
     console.log("App Started...");
     
-    // create a reference to a p element with an id of "firstParagraph"
-    var firstParagraph = document.getElementById("firstParagraph");
     
-    firstParagraph.textContent = "It's alive!!!";
+    /*
+    // declare named function
+    function writeToFirstParagraph() {
+        // for debugging only
+        console.log("Executing writeToFirstParagraph");
+        
+        // create a reference to a p element with an id of "firstParagraph"
+        var firstPTag = document.getElementById("firstParagraph");
+    
+        firstPTag.textContent = "It's alive!!!";
+    }
+    */
+    
+    // declare a variable that points to anonymous function
+    var writeToFirstParagraph = function() {
+        // for debugging only
+        console.log("Executing writeToFirstParagraph");
+        
+        // create a reference to a p element with an id of "firstParagraph"
+        var firstPTag = document.getElementById("firstParagraph");
+    
+        firstPTag.textContent = "It's alive!!!";
+    } 
+   
     
     
+    // execute function
+    writeToFirstParagraph();
     
+    console.log(firstParagraph);
     
     
 })();
